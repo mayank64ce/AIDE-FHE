e1450f45b94666bbbd1550ece00e644dfead3f09
mayank64ce <ma982513@ucf.edu>
2026-03-09 13:23:25 -0400

    added experiment script

--- challenge.sh
+++ challenge.sh
@@ -41,6 +41,50 @@
 # aide-fhe challenge_dir=/home/yifei/data/FHE-Bench/aideml/fhe_challenge/white_box/openfhe/challenge_array_sorting agent.steps=50 agent.search.num_drafts=10 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
 # aide-fhe challenge_dir=/home/yifei/data/FHE-Bench/aideml/fhe_challenge/white_box/openfhe/challenge_lookup_table agent.steps=50 agent.search.num_drafts=10 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
 
-aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/AIDE-FHE/fhe_challenge/black_box/challenge_sign agent.steps=10 agent.search.num_drafts=5 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
-aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/AIDE-FHE/fhe_challenge/black_box/challenge_relu agent.steps=10 agent.search.num_drafts=5 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
-aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/AIDE-FHE/fhe_challenge/black_box/challenge_sigmoid agent.steps=10 agent.search.num_drafts=5 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/AIDE-FHE/fhe_challenge/black_box/challenge_sign agent.steps=10 agent.search.num_drafts=5 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/AIDE-FHE/fhe_challenge/black_box/challenge_relu agent.steps=10 agent.search.num_drafts=5 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/AIDE-FHE/fhe_challenge/black_box/challenge_sigmoid agent.steps=10 agent.search.num_drafts=5 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
+
+## WHITE_BOX CHALLENGES ##
+
+# gpt-4o-mini
+
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/ml_inference/challenge_cifar10 agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-4o-mini agent.code.reasoning_effort=high
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/ml_inference/challenge_sentiment_analysis agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-4o-mini agent.code.reasoning_effort=high
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/ml_inference/challenge_house_prediction agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-4o-mini agent.code.reasoning_effort=high
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/ml_inference/challenge_svm agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-4o-mini agent.code.reasoning_effort=high
+
+
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/openfhe/challenge_gelu agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-4o-mini agent.code.reasoning_effort=high
+aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/openfhe/challenge_softmax agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-4o-mini agent.code.reasoning_effort=high
+aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/openfhe/challenge_shl agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-4o-mini agent.code.reasoning_effort=high
+aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/openfhe/challenge_array_sorting agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-4o-mini agent.code.reasoning_effort=high
+aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/openfhe/challenge_lookup_table agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-4o-mini agent.code.reasoning_effort=high
+aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/openfhe/challenge_matrix_multiplication agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-4o-mini agent.code.reasoning_effort=high
+aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/openfhe/challenge_max agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-4o-mini agent.code.reasoning_effort=high
+aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/openfhe/challenge_knn agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-4o-mini agent.code.reasoning_effort=high
+aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/openfhe/challenge_parity agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-4o-mini agent.code.reasoning_effort=high
+aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/openfhe/challenge_invertible_matrix agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-4o-mini agent.code.reasoning_effort=high
+aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/openfhe/challenge_svd agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-4o-mini agent.code.reasoning_effort=high
+
+
+
+# gpt-5-mini
+
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/ml_inference/challenge_cifar10 agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/ml_inference/challenge_sentiment_analysis agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/ml_inference/challenge_house_prediction agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/ml_inference/challenge_svm agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
+
+
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/openfhe/challenge_gelu agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/openfhe/challenge_softmax agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/openfhe/challenge_shl agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/openfhe/challenge_array_sorting agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/openfhe/challenge_lookup_table agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/openfhe/challenge_matrix_multiplication agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/openfhe/challenge_max agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/openfhe/challenge_knn agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/openfhe/challenge_parity agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/openfhe/challenge_invertible_matrix agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
+# aide-fhe challenge_dir=/home/mayank/Documents/Code/Project/FHE-agent-bench/fhe_challenge/white_box/openfhe/challenge_svd agent.steps=10 agent.search.num_drafts=3 agent.code.model=gpt-5-mini-2025-08-07 agent.code.reasoning_effort=high
